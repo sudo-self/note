ad6ec2835f79de12fcc13b373e643eba3b6c8fad
sudo-self <sudo-self@proton.me>
2025-09-05 03:22:47 -0600

    Update index.get.ts

--- server/api/todos/index.get.ts
+++ server/api/todos/index.get.ts
@@ -3,8 +3,19 @@ import { eq } from 'drizzle-orm'
 export default eventHandler(async (event) => {
   const { user } = await requireUserSession(event)
 
-  // List todos for the current user
-  const todos = await useDB().select().from(tables.todos).where(eq(tables.todos.userId, user.id)).all()
+
+  const todos = await useDB()
+    .select({
+      id: tables.todos.id,
+      title: tables.todos.title,
+      completed: tables.todos.completed,
+      slug: tables.todos.slug,      
+      createdAt: tables.todos.createdAt 
+    })
+    .from(tables.todos)
+    .where(eq(tables.todos.userId, user.id))
+    .all()
 
   return todos as Todo[]
 })
+
