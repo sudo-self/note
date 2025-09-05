0c8f8d7daa80c861d4855dfbb4e53d8fe1d51949
sudo-self <sudo-self@proton.me>
2025-09-05 03:24:54 -0600

    Update 0000_heavy_xorn.sql

--- .data/hub/database/migrations/0000_heavy_xorn.sql
+++ .data/hub/database/migrations/0000_heavy_xorn.sql
@@ -1,7 +1,9 @@
 CREATE TABLE `todos` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`user_id` integer NOT NULL,
-	`title` text NOT NULL,
-	`completed` integer DEFAULT 0 NOT NULL,
-	`created_at` integer NOT NULL
+  `id` integer PRIMARY KEY NOT NULL,
+  `user_id` integer NOT NULL,
+  `title` text NOT NULL,
+  `completed` integer DEFAULT 0 NOT NULL,
+  `created_at` integer NOT NULL,
+  `slug` text UNIQUE NOT NULL
 );
+
